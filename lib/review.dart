67bf5be78ad233f0c635cc295280c4300c5a004b
Wiky Lyu <wikylyu@outlook.com>
2023-09-09 10:12:35 +0800

    review opencount

--- lib/review.dart
+++ lib/review.dart
@@ -4,11 +4,11 @@ import 'package:xyz_utils/sharedpref.dart';
 
 final InAppReview inAppReview = InAppReview.instance;
 
-checkAndRequestReview() async {
+checkAndRequestReview({int openCount = 8}) async {
   final count = SharedPreferencesService.prefs.getInt("open_count") ?? 0;
   final showed =
       SharedPreferencesService.prefs.getBool("show_reviewed") ?? false;
-  if (count >= 8 && !showed) {
+  if (count >= openCount && !showed) {
     try {
       if (await inAppReview.isAvailable()) {
         await inAppReview.requestReview();

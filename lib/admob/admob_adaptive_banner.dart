20b9bb5a35a212b901c8f4dc9fbefe2e45db6ca9
Wiky Lyu <wikylyu@outlook.com>
2023-09-26 23:02:46 +0800

    adaptive banner

---
+++ lib/admob/admob_adaptive_banner.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:xyz_utils/admob/config.dart';
+
+class AdmobAdaptiveBanner extends StatefulWidget {
+  const AdmobAdaptiveBanner({Key? key}) : super(key: key);
+
+  @override
+  createState() => _AdmobAdaptiveBannerState();
+}
+
+class _AdmobAdaptiveBannerState extends State<AdmobAdaptiveBanner> {
+  BannerAd? _anchoredAdaptiveAd;
+  bool _isLoaded = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _loadAd();
+  }
+
+  Future<void> _loadAd() async {
+    final AnchoredAdaptiveBannerAdSize? size =
+        await AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(
+            MediaQuery.of(context).size.width.truncate());
+
+    if (size == null) {
+      debugPrint('Unable to get height of anchored banner.');
+      return;
+    }
+
+    _anchoredAdaptiveAd = BannerAd(
+      adUnitId: bannerAdID,
+      size: size,
+      request: AdManagerAdRequest(
+        keywords: adKeywords,
+      ),
+      listener: BannerAdListener(
+        onAdLoaded: (Ad ad) {
+          debugPrint('$ad loaded: ${ad.responseInfo}');
+          setState(() {
+            _anchoredAdaptiveAd = ad as BannerAd;
+            _isLoaded = true;
+          });
+        },
+        onAdFailedToLoad: (Ad ad, LoadAdError error) {
+          debugPrint('Anchored adaptive banner failedToLoad: $error');
+          ad.dispose();
+        },
+      ),
+    );
+    return _anchoredAdaptiveAd!.load();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_anchoredAdaptiveAd == null) {
+      return Container();
+    }
+    return Container(
+      margin: const EdgeInsets.only(top: 5, bottom: 5),
+      alignment: Alignment.center,
+      width: _anchoredAdaptiveAd!.size.width.toDouble(),
+      height: _anchoredAdaptiveAd!.size.height.toDouble(),
+      child: _isLoaded ? AdWidget(ad: _anchoredAdaptiveAd!) : null,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _anchoredAdaptiveAd?.dispose();
+  }
+}

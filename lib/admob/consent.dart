d8cb274b867b64a0b77f33895555d4d6840588d7
Wiky Lyu <wikylyu@outlook.com>
2023-10-14 19:33:14 +0800

    consent

---
+++ lib/admob/consent.dart
@@ -0,0 +1,42 @@
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+void loadForm() {
+  ConsentForm.loadConsentForm(
+    (ConsentForm consentForm) async {
+      var status = await ConsentInformation.instance.getConsentStatus();
+      if (status == ConsentStatus.required) {
+        consentForm.show(
+          (FormError? formError) {
+            loadForm();
+          },
+        );
+      }
+    },
+    (FormError formError) {},
+  );
+}
+
+void setConsentEea() {}
+
+loadConsentInfoForm({bool testing = false}) async {
+  final ConsentRequestParameters params;
+  if (testing) {
+    ConsentDebugSettings debugSettings = ConsentDebugSettings(
+      debugGeography: DebugGeography.debugGeographyEea,
+    );
+    params = ConsentRequestParameters(
+      consentDebugSettings: debugSettings,
+    );
+  } else {
+    params = ConsentRequestParameters();
+  }
+  ConsentInformation.instance.requestConsentInfoUpdate(
+    params,
+    () async {
+      if (await ConsentInformation.instance.isConsentFormAvailable()) {
+        loadForm();
+      }
+    },
+    (FormError error) {},
+  );
+}
